c9c329adf9226c4ec7c81ce505ffddf38a99c357
Nahidef <erdincfidan18@gmail.com>
2025-05-03 23:08:43 +0300

    Add project files.

---
+++ BPCS_Steganography/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BPCS_Steganography
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtMessage = new System.Windows.Forms.TextBox();
+			this.btnEmbed = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+			this.lblExtractedMessage = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtMessage
+			// 
+			this.txtMessage.Location = new System.Drawing.Point(265, 241);
+			this.txtMessage.Name = "txtMessage";
+			this.txtMessage.Size = new System.Drawing.Size(248, 22);
+			this.txtMessage.TabIndex = 0;
+			// 
+			// btnEmbed
+			// 
+			this.btnEmbed.Location = new System.Drawing.Point(265, 307);
+			this.btnEmbed.Name = "btnEmbed";
+			this.btnEmbed.Size = new System.Drawing.Size(99, 23);
+			this.btnEmbed.TabIndex = 1;
+			this.btnEmbed.Text = "Mesajı Göm";
+			this.btnEmbed.UseVisualStyleBackColor = true;
+			this.btnEmbed.Click += new System.EventHandler(this.btnEmbed_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(406, 307);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(107, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Mesajı Çıkar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.btnExtract_Click);
+			// 
+			// pictureBoxImage
+			// 
+			this.pictureBoxImage.Location = new System.Drawing.Point(265, 12);
+			this.pictureBoxImage.Name = "pictureBoxImage";
+			this.pictureBoxImage.Size = new System.Drawing.Size(248, 191);
+			this.pictureBoxImage.TabIndex = 3;
+			this.pictureBoxImage.TabStop = false;
+			// 
+			// lblExtractedMessage
+			// 
+			this.lblExtractedMessage.AutoSize = true;
+			this.lblExtractedMessage.Location = new System.Drawing.Point(351, 382);
+			this.lblExtractedMessage.Name = "lblExtractedMessage";
+			this.lblExtractedMessage.Size = new System.Drawing.Size(44, 16);
+			this.lblExtractedMessage.TabIndex = 4;
+			this.lblExtractedMessage.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(824, 494);
+			this.Controls.Add(this.lblExtractedMessage);
+			this.Controls.Add(this.pictureBoxImage);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.btnEmbed);
+			this.Controls.Add(this.txtMessage);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtMessage;
+		private System.Windows.Forms.Button btnEmbed;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.PictureBox pictureBoxImage;
+		private System.Windows.Forms.Label lblExtractedMessage;
+	}
+}
+
---
+++ BPCS_Steganography/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BPCS_Steganography
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnEmbed_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog ofd = new OpenFileDialog())
+			{
+				ofd.Filter = "Image Files (*.png)|*.png|JPEG Files (*.jpg;*.jpeg)|*.jpg;*.jpeg";
+				if (ofd.ShowDialog() == DialogResult.OK)
+				{
+					try
+					{
+						string inputFilePath = ofd.FileName;
+						string message = txtMessage.Text;
+						string outputFilePath = Path.Combine(
+							Path.GetDirectoryName(inputFilePath),
+							"stego_" + Path.GetFileName(inputFilePath));
+
+						using (Bitmap inputImage = new Bitmap(inputFilePath))
+						{
+							Bitmap outputImage = BPCSSteganography.EmbedMessage(inputImage, message);
+							outputImage.Save(outputFilePath, System.Drawing.Imaging.ImageFormat.Png);
+							MessageBox.Show("Message embedded successfully in PNG format");
+						}
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show($"Error: {ex.Message}", "Embedding Failed",
+									  MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+		private void btnExtract_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog ofd = new OpenFileDialog())
+			{
+				ofd.Filter = "Image Files (*.png)|*.png|JPEG Files (*.jpg;*.jpeg)|*.jpg;*.jpeg";
+				if (ofd.ShowDialog() == DialogResult.OK)
+				{
+					try
+					{
+						using (Bitmap inputImage = new Bitmap(ofd.FileName))
+						{
+							pictureBoxImage.Image = new Bitmap(inputImage); 
+
+							string extractedMessage = BPCSSteganography.ExtractMessage(inputImage);
+							lblExtractedMessage.Text = "Extracted Message: " + extractedMessage;
+						}
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show($"Error: {ex.Message}", "Extraction Failed",
+									  MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+	}
+
+	public static class BPCSSteganography
+	{
+		private const int LENGTH_HEADER_SIZE = 4; 
+
+		public static Bitmap EmbedMessage(Bitmap inputImage, string message)
+		{
+			byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+			int messageLength = messageBytes.Length;
+
+			
+			int maxCapacity = (inputImage.Width * inputImage.Height) / 8 - LENGTH_HEADER_SIZE;
+			if (messageLength > maxCapacity)
+				throw new Exception($"Message too large. Max capacity: {maxCapacity} bytes");
+
+			Bitmap outputImage = new Bitmap(inputImage);
+
+			
+			byte[] lengthBytes = BitConverter.GetBytes(messageLength);
+			EmbedBytes(outputImage, lengthBytes, 0);
+
+			
+			EmbedBytes(outputImage, messageBytes, LENGTH_HEADER_SIZE * 8);
+
+			return outputImage;
+		}
+
+		private static void EmbedBytes(Bitmap image, byte[] bytes, int startBitIndex)
+		{
+			int width = image.Width;
+			int totalBits = bytes.Length * 8;
+
+			for (int i = 0; i < totalBits; i++)
+			{
+				int bitIndex = startBitIndex + i;
+				int x = bitIndex % width;
+				int y = bitIndex / width;
+
+				if (y >= image.Height) break;
+
+				Color pixel = image.GetPixel(x, y);
+
+				
+				int bitValue = (bytes[i / 8] >> (7 - (i % 8))) & 1;
+
+				int newR = (pixel.R & 0xFE) | bitValue;
+				image.SetPixel(x, y, Color.FromArgb(newR, pixel.G, pixel.B));
+			}
+		}
+
+		public static string ExtractMessage(Bitmap inputImage)
+		{
+			
+			int messageLength = ExtractInt(inputImage, 0);
+			if (messageLength <= 0 || messageLength > (inputImage.Width * inputImage.Height / 8) - LENGTH_HEADER_SIZE)
+				throw new Exception("Invalid message length detected");
+
+			
+			byte[] messageBytes = new byte[messageLength];
+			ExtractBytes(inputImage, messageBytes, LENGTH_HEADER_SIZE * 8);
+
+			return Encoding.UTF8.GetString(messageBytes);
+		}
+
+		private static int ExtractInt(Bitmap image, int startBitIndex)
+		{
+			byte[] buffer = new byte[4];
+			ExtractBytes(image, buffer, startBitIndex);
+			return BitConverter.ToInt32(buffer, 0);
+		}
+
+		private static void ExtractBytes(Bitmap image, byte[] buffer, int startBitIndex)
+		{
+			int width = image.Width;
+			int totalBits = buffer.Length * 8;
+
+			for (int i = 0; i < totalBits; i++)
+			{
+				int bitIndex = startBitIndex + i;
+				int x = bitIndex % width;
+				int y = bitIndex / width;
+
+				if (y >= image.Height) break;
+
+				Color pixel = image.GetPixel(x, y);
+				int bitValue = pixel.R & 1;
+
+				buffer[i / 8] |= (byte)(bitValue << (7 - (i % 8)));
+			}
+		}
+	}
+}
